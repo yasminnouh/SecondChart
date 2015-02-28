7c2001d197df499cc74e12399b8ea3f3fa55aac1
Yasmin Nouh <yasminnouh@Yasmins-MacBook-Pro.local>
2015-02-28 14:27:13 -0500

    Committing GoogleStylesObs file.

---
+++ GoogleStyleObs.js
@@ -0,0 +1,12 @@
+
+var myTextStyle = { //taken the thing that was repeated and made it into its own object 
+	fontName : 'Times-Roman',
+	fontSize : 18,
+	bold : true,
+	italic : true,
+	color : '#871b47', // The color of the text.
+	auraColor : '#d799ae', // The color of the text outline.
+	opacity : 0.8 // The transparency of the text.
+};
+//as long as you include this in html page before scipts fil
+// above is a list of key and value pairs
